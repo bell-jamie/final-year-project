8e6a019d9e148b986625aec969277ce8339bf936
James Bell <jamie2001bell@gmail.com>
2024-02-12 23:32:28 +0000

    Quick additions to short syntax file

--- single-edge-notched-plate/nl-segregated-short.py
+++ single-edge-notched-plate/nl-segregated-short.py
@@ -7,18 +7,18 @@ nu = 0.3
 filename_mesh = data_dir + '/notchedPlateTriangular.mesh'
 
 regions = {
-    'Omega' : 'all',
-    'Top' : '', # find all the vertices on the top of the plate (x >= 0.5)
-    'Bottom' : '', # find all the vertices on the bottom of the plate (x <= 0.5)
+    'omega' : 'all',
+    'top' : '', # find all the vertices on the top of the plate (x >= 0.5)
+    'bottom' : '', # find all the vertices on the bottom of the plate (x <= 0.5)
 }
 
 materials = {
-    'Solid' : ({'D' : stiffness_from_youngpoisson(E, nu)}),
+    'solid' : ({'D' : stiffness_from_youngpoisson(E, nu)}),
 }
 
 fields = {
-    'Displacement' : ('real', 'vector', 'Omega', 1),
-    'Damage' : ('real', 'scalar', 'Omega', 1),
+    'displacement' : ('real', 'vector', 'Omega', 1),
+    'phase-field' : ('real', 'scalar', 'Omega', 1),
 }
 
 equations = {
@@ -27,5 +27,20 @@ equations = {
 }
 
 variables = {
-    
+    'u_pf' : ('unknown field', 'phase-field', 0), # is damage being solved first? 0 -> 1
+    'v_pf' : ('test field', 'phase-field', 'u_pf'),
+    'u' : ('unknown field', 'displacement', 1),
+    'v' : ('test field', 'displacement', 'u'),
+}
+
+ebcs = {
+    'fixed' : ('Bottom', {'u.0' : 0.0, 'u.1' : 0.0}), # fix x and y of the bottom vertices
+}
+
+solvers = {
+    'ls' : ('ls.scipy_direct', {}),
+    'newton' : ('nls.newton', {
+        'i_max' : 1, # maximum number of iterations - make this equal to Julia code
+        'eps_a' : 1e-6, # res norm tolerance
+    }),
 }

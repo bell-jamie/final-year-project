e7c4787ec2d9b5c8f07088359d6d279c20d962f9
James Bell <jamie2001bell@gmail.com>
2024-04-27 23:32:54 +0100

    Script logging and auto plots added. Refactored material hook.

--- single-edge-notched-plate/nl-coupled-multi-field.py
+++ single-edge-notched-plate/nl-coupled-multi-field.py
@@ -10,6 +10,7 @@ from sfepy.mechanics.tensors import get_von_mises_stress
 
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 
@@ -32,31 +33,29 @@ def material(ts, coors, mode=None, **kwargs):
     Returns the original stiffness tensor and the modified stiffness tensor.
     Also returns the Griffith criterion multiplied and divided by the length scale.
     """
-    if mode == "qp":
-        pb = kwargs["problem"]
+    if mode != "qp":
+        return
 
-        stn = (-1, 3, 1)
-        dam = (-1, 1, 1)
+    pb = kwargs["problem"]
+    ev = pb.evaluate
 
-        strain = pb.evaluate("ev_cauchy_strain.i.Omega(u_disp)", mode="qp").reshape(stn)
-        damage = pb.evaluate("ev_integrate.i.Omega(u_phase)", mode="qp").reshape(dam)
+    strain = ev("ev_cauchy_strain.i.Omega(u_disp)", mode="qp").reshape((-1, 3, 1))
+    damage = ev("ev_integrate.i.Omega(u_phase)", mode="qp").reshape((-1, 1, 1))
 
-        cns = (1, 1, 1)
-        dim = (strain.shape[0], 1, 1)
+    c = CMAT
+    c_dev = tensors.get_deviator(c)
+    c_vol = tensors.get_volumetric_tensor(c)
+    gcls = np.full((1, 1, 1), GC * LS)
+    gc_ls = np.full((1, 1, 1), GC / LS)
 
-        c = CMAT
-        c_dev = tensors.get_deviator(c)
-        c_vol = tensors.get_volumetric_tensor(c)
-        c_mod = np.zeros((strain.shape[0] * strain.shape[1], c.shape[0], c.shape[1]))
-        gcls = np.full(cns, GC * LS)
-        gc_ls = np.full(cns, GC / LS)
+    trace = (strain[:, 0, 0] + strain[:, 1, 0]).reshape((len(strain), 1, 1))
+    damage_factor = damage**2 + ETA
 
-        trace = strain[:, 0, 0] + strain[:, 1, 0]
-        c_tens = (damage[:, 0, 0] ** 2 + ETA).reshape(dim) * c
-        c_comp = (damage[:, 0, 0] ** 2 + ETA).reshape(dim) * c_dev + c_vol
-        c_mod = np.where(trace.reshape(dim) >= 0, c_tens, c_comp)
+    c_tens = damage_factor * c
+    c_comp = damage_factor * c_dev + c_vol
+    c_mod = np.where(trace >= 0, c_tens, c_comp)
 
-        return {"C": c_mod, "GCLS": gcls, "GC_LS": gc_ls}
+    return {"C": c_mod, "GCLS": gcls, "GC_LS": gc_ls}
 
 
 def energy(ts, coors, mode=None, **kwargs):
@@ -75,16 +74,16 @@ def energy(ts, coors, mode=None, **kwargs):
         return tensor
 
     pb = kwargs["problem"]
+    ev = pb.evaluate
     phi_old = pb.phi
 
-    s = (-1, 3, 1)
-
-    strain = pb.evaluate("ev_cauchy_strain.i.Omega(u_disp)", mode="qp").reshape(s)
-    stress = pb.evaluate("ev_cauchy_stress.i.Omega(c.C, u_disp)", mode="qp").reshape(s)
+    strain = ev("ev_cauchy_strain.i.Omega(u_disp)", mode="qp").reshape((-1, 3, 1))
+    stress = ev("ev_cauchy_stress.i.Omega(c.C, u_disp)", mode="qp").reshape((-1, 3, 1))
 
     trace = strain[:, 0, 0] + strain[:, 1, 0]
     product = np.einsum("ijk, ijk -> i", stress, strain)
     dev_product = np.einsum("ijk, ijk -> i", dev(stress), dev(strain))
+
     phi_new = np.where(trace >= 0, product, dev_product).reshape(phi_old.shape)
     phi = np.maximum(phi_old, phi_new)
     pb.phi = phi
@@ -94,45 +93,47 @@ def energy(ts, coors, mode=None, **kwargs):
 
 def time_step(ts, status, adt, pb, verbose=False):
     """
-    Sets the time step (load displacement) based on the current displacement.
-    Also saves the displacement, damage and elastic strain energy at the current time step.
-    If the solver residual is above a certain tolerance, the time step is halved and the field history is recovered.
+    Just a placeholder to allow the adaptive timestepper to be manually controlled.
     """
-    print("Time step hook: setting displacement.")
-    u = pb.ebcs[1].dofs["u_disp.1"]
-
-    # if status.err > TOL:
-    #     # Backtrack
-    #     u -= pb.step  # remove the step
-    #     pb.step /= 2  # halve the step
-
-    #     # Recover from field history
-    #     pb.get_variables()["u_disp"].data[0] = pb.history.u_disp
-    #     pb.get_variables()["u_phase"].data[0] = pb.history.u_phase
-    #     pb.phi = pb.history.phi
-    # else:
-    # Default step size
-    if u < 5e-3:
-        pb.step = 1e-4
-    else:
-        pb.step = 5e-6
-
-    # if u < 3e-3:
-    #     pb.step = 1e-3
-    # elif u < 5e-3:
-    #     pb.step = 1e-4
-    # else:
-    #     pb.step = 1e-5
-
-    # Save field history
-    pb.history.u_disp = pb.get_variables()["u_disp"].data[0]
-    pb.history.u_phase = pb.get_variables()["u_phase"].data[0]
-    pb.history.phi = pb.phi
-
-    # Update the displacement
-    ts.time = pb.ebcs[1].dofs["u_disp.1"] = u + pb.step
-
     return True
+
+
+# def time_step(ts, status, adt, pb, verbose=False):
+#     """
+#     Sets the time step (load displacement) based on the current displacement.
+#     Also saves the displacement, damage and elastic strain energy at the current time step.
+#     If the solver residual is above a certain tolerance, the time step is halved and the field history is recovered.
+#     """
+#     return True
+
+# print("Time step hook: setting displacement.")
+# u = pb.ebcs[1].dofs["u_disp.1"]
+
+# # if status.err > TOL:
+# #     # Backtrack
+# #     u -= pb.step  # remove the step
+# #     pb.step /= 2  # halve the step
+
+# #     # Recover from field history
+# #     pb.get_variables()["u_disp"].data[0] = pb.history.u_disp
+# #     pb.get_variables()["u_phase"].data[0] = pb.history.u_phase
+# #     pb.phi = pb.history.phi
+# # else:
+# # Default step size
+
+# # Get next time step
+# if steps and u >= steps[0][0]:
+#     pb.step = steps.pop(0)[1]
+
+# # # Save field history
+# # pb.history.u_disp = pb.get_variables()["u_disp"].data[0]
+# # pb.history.u_phase = pb.get_variables()["u_phase"].data[0]
+# # pb.history.phi = pb.phi
+
+# # Update the displacement
+# ts.time = pb.ebcs[1].dofs["u_disp.1"] = u + pb.step
+
+# return True
 
 
 def pre_process(pb):
@@ -149,18 +150,20 @@ def pre_process(pb):
         case _:
             raise ValueError("Unsupported element type.")
 
-    pb.phi = np.zeros((pb.domain.mesh.n_el * quad, 1, 1))
     pb.step = 0
+    pb.exit = 0
+    pb.phi = np.zeros((pb.domain.mesh.n_el * quad, 1, 1))
+    pb.log = IndexedStruct(disp=[], force=[], damage=[], energy=[])
     pb.history = IndexedStruct
 
 
-def step_hook(pb, ts, variables):
-    """
-    Gets called after each step, right before the post process hook.
-    """
-    pass
-    print("Step hook: updating load boundary condition.")
-    pb.ebcs[1].dofs["u_disp.1"] = ts.time
+# def step_hook(pb, ts, variables):
+#     """
+#     Gets called after each step, right before the post process hook.
+#     """
+#     pass
+#     print("Step hook: updating load boundary condition.")
+#     pb.ebcs[1].dofs["u_disp.1"] = ts.time
 
 
 def nls_iter_hook(pb, nls, vec, it, err, err0):
@@ -196,10 +199,19 @@ def post_process(out, pb, state, extend=False):
     # Force - [[xx], ->[yy]<-, [2xy]
     force = ev("ev_cauchy_stress.i.Load(m.C, u_disp)", mode="eval")[1]
 
+    # Displacement step
+    pb.step = steps.pop(0)[1] if steps and disp >= steps[0][0] else pb.step
+    pb.ts.time = pb.ebcs[1].dofs["u_disp.1"] = disp + pb.step
+
     # Write to log
     with open(os.path.join(save_directory, "log.csv"), mode="a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow([disp, force, damage_sum, energy_sum])
+
+    pb.log.disp.append(disp)
+    pb.log.force.append(force)
+    pb.log.damage.append(damage_sum)
+    pb.log.energy.append(energy_sum)
 
     # Display stats
     print(f"\n############### STATS ###############")
@@ -210,15 +222,42 @@ def post_process(out, pb, state, extend=False):
     print(f"Energy: {energy_sum}")
     print(f"#####################################\n")
 
+    # Exit criteria
+    if disp >= DEX:
+        print("Displacement exit criteria met.")
+        pb.ts.time = T1
+
+    if force < FEX[0] and pb.exit + FEX[1] < disp:
+        print("Force exit criteria met.")
+        pb.ts.time = T1
+    elif force > FEX[0]:
+        pb.exit = disp
+
     return out
+
+
+def post_process_final(problem, state):
+    """
+    Used for creating force-displacement plot.
+    """
+    fig, ax = plt.subplots()
+    ax.plot(problem.log.disp, problem.log.force)
+    ax.set_xlabel("Displacement [mm]")
+    ax.set_ylabel("Force [N]")
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(save_directory, "force_displacement.png"))
 
 
 # Constants (SI units, with mm as base length unit)
 T0 = 0.0  # Initial time (always 0)
-T1 = 7.5e-3 + 1e-3  # Final displacement (always add 1e-3 because of adaptive solver)
+T1 = 1.0  # Arbitrary final time
+# T1 = 7.5e-3 + 1e-3  # Final displacement (always add 1e-3 because of adaptive solver)
 DT = 2.5e-3  # Initial time step -- NOT USED
 TOL = 1e-8  # Tolerance for the nonlinear solver
 IMAX = 10  # Maximum number of solver iterations
+FEX = (1.0, 5e-4)  # Exit force requirement (N) over which displacement (mm)
+DEX = 10e-3  # Exit displacement requirement (mm) - should this be very large for sensitivity study?
 
 E = 210e3  # Young's modulus (MPa)
 NU = 0.3  # Poisson's ratio
@@ -231,9 +270,14 @@ CMAT = matcoefs.stiffness_from_youngpoisson(dim=2, young=E, poisson=NU, plane="s
 ORDER = 2
 DEGREE = 2 * ORDER
 
+steps = [
+    [0, 1e-4],
+    [5e-3, 1e-5],
+]
+
 current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 script_directory = os.path.dirname(__file__)
-filename_mesh = os.path.join(script_directory, "meshes", "notchedPlateRahaman.vtk")
+filename_mesh = os.path.join(script_directory, "meshes", "notchedPlateTriangular.vtk")
 save_directory = os.path.join(
     script_directory,
     "files",
@@ -241,9 +285,19 @@ save_directory = os.path.join(
     current_datetime,
 )
 os.makedirs(save_directory, exist_ok=True)
+
+# Create log file
 with open(os.path.join(save_directory, "log.csv"), mode="w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["Displacement", "Force", "Damage", "Energy"])
+
+# Read script contents
+with open(os.path.realpath(__file__), mode="r") as file:
+    script_content = file.read()
+
+# Save script contents
+with open(os.path.join(save_directory, "nl-coupled-multi-field.py"), mode="w") as file:
+    file.write(script_content)
 
 options = {
     "nls": "newton",
@@ -251,10 +305,10 @@ options = {
     "output_dir": save_directory,
     "pre_process_hook": "pre_process",
     "nls_iter_hook": "nls_iter_hook",
-    "step_hook": "step_hook",
-    #'parametric_hook' : 'parametric_hook', - Can be used to programmatically change problem
+    # "step_hook": "step_hook",
+    # 'parametric_hook' : 'parametric_hook', - Can be used to programmatically change problem
     "post_process_hook": "post_process",
-    # "post_process_hook_final": "post_process_hook_final",
+    "post_process_hook_final": "post_process_final",
     "save_times": "all",
     # "save_times": 100,
 }
@@ -262,11 +316,11 @@ options = {
 regions = {
     "Omega": "all",
     "Load": (
-        "vertices in (y > 0.99)",
+        "vertices in (y > 0.49)",
         "facet",
     ),
     "Fixed": (
-        "vertices in (y < 0.01)",
+        "vertices in (y < -0.49)",
         "facet",
     ),
     # "Force": (
